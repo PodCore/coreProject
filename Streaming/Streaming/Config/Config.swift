882e1ab82bfaf6c374d4ae23c5a67ad90325d940
Sky Xu <skyrocks88xmz@gmail.com>
2018-01-22 15:19:50 -0800

    finish home screen and click cell to watchView

---
+++ Streaming/Streaming/Config/Config.swift
@@ -0,0 +1,16 @@
+//
+//  Config.swift
+//  Streaming
+//
+//  Created by Sky Xu on 1/18/18.
+//  Copyright © 2018 Sky Xu. All rights reserved.
+//
+
+import Foundation
+
+struct Config {
+    // MARK: Fix me! rtmp url doesn't work
+    static var rtmpPushUrl = "rtmp://106.187.100.229/live/"
+    static var rtmpPlayUrl = "rtmp://106.187.100.229/live/"
+    static var serverUrl = "https://podcore.herokuapp.com"
+}
---
+++ Streaming/Streaming/ViewController/WatchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WatchViewController.swift
+//  
+//
+//  Created by Sky Xu on 1/22/18.
+//
+
+import UIKit
+
+class WatchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
