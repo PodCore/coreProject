d471314c502d22d36c6e554189cffebeff93c2cb
Sky Xu <skyrocks88xmz@gmail.com>
2018-02-05 10:43:20 -0800

    add sample app

---
+++ sampleApp/SDKSampleApp.swift/WatchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WatchViewController.swift
+//  SwiftSDKSampleApp
+//
+//  Created by Sky Xu on 2/4/18.
+//  Copyright © 2018 Wowza, Inc. All rights reserved.
+//
+
+import UIKit
+
+class WatchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
