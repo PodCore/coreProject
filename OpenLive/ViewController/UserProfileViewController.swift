1d92d46498539270a883e9e3a93f64f21d899b5e
Tony Cioara <tonyangelo9707@gmail.com>
2018-02-10 16:10:43 -0800

    worked on user profile. TODO: Connect to backend

---
+++ OpenLive/ViewController/UserProfileViewController.swift
@@ -0,0 +1,27 @@
+//
+//  UserProfileViewController.swift
+//  OpenLive
+//
+//  Created by Tony Cioara on 2/10/18.
+//  Copyright © 2018 Agora. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
