15edf347d482f8bcd363955b55068e59df24eae4
Tony Cioara <tonyangelo9707@gmail.com>
2018-02-27 11:03:02 -0800

    Deleted useless folders

--- Streaming/PlayLiveVideo.swift
+++
@@ -1,9 +0,0 @@
-//
-//  PlayLiveVideo.swift
-//  Streaming
-//
-//  Created by Sky Xu on 1/18/18.
-//  Copyright © 2018 Sky Xu. All rights reserved.
-//
-
-import Foundation
--- sampleApp/SDKSampleApp.swift/UYLNotificationDelegate.swift
+++
@@ -1,8 +0,0 @@
-//
-//  UYLNotificationDelegate.swift
-//  MySampleApp
-//
-//  Created by Sky Xu on 2/5/18.
-//
-
-import Foundation
