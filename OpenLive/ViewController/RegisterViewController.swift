09800783963b7786de0106b25ba7bb2b64778bb7
tsukudabuddha <andrewtsukuda@gmail.com>
2018-03-27 15:01:06 -0700

    Added Login VC and fixed register

--- OpenLive/ViewController/RegisterViewController.swift
+++ OpenLive/ViewController/RegisterViewController.swift
@@ -4,7 +4,9 @@
 //
 //  Created by Sky Xu on 1/23/18.
 //  Copyright © 2018 Sky Xu. All rights reserved.
-//
+// TODO: Delete login stuff after connecting SB to LoginVC
+// TODO: Add popup view when same username
+
 
 import UIKit
 import FacebookLogin
@@ -12,7 +14,6 @@ import FBSDKLoginKit
 import FBSDKCoreKit
 import GoogleSignIn
 import Google
-//import RNCryptor
 
 class RegisterViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate  {
     
@@ -20,9 +21,9 @@ class RegisterViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDe
     var loginEmail: String?
     let defaults = UserDefaults.standard
     
-    @IBOutlet weak var username: UITextField!
-    @IBOutlet weak var email: UITextField!
-    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var regularButton: UIButton!
     @IBOutlet weak var fbButton: UIButton!
     @IBOutlet weak var gmailButton: UIButton!
@@ -39,17 +40,17 @@ class RegisterViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDe
     }
     
     //    MARK: put these into view did load of profile VC
-//     NotificationCenter.default.addObserver(self, selector: #selector(ProfileViewController.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
-//
-//    @objc func userDataDidChange(_ notif: Notification) {
-//        if AuthService.instance.isLoggedIn {
-////            update userinfo if user is logged in
-//            let username = UserdataService.instance.username
-//            let avatar = UserdataService.instance.avatar
-//        } else {
-////            set UI to be default value
-//        }
-//    }
+    //     NotificationCenter.default.addObserver(self, selector: #selector(ProfileViewController.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+    //
+    //    @objc func userDataDidChange(_ notif: Notification) {
+    //        if AuthService.instance.isLoggedIn {
+    ////            update userinfo if user is logged in
+    //            let username = UserdataService.instance.username
+    //            let avatar = UserdataService.instance.avatar
+    //        } else {
+    ////            set UI to be default value
+    //        }
+    //    }
     
     
     //   MARK: (helper) main func to call google Signin
@@ -58,12 +59,12 @@ class RegisterViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDe
             print(error)
             return
         }
-        AuthService.instance.registerUser(username: user.profile.givenName, email: user.profile.email, password: "gmailpassword ", completion: {[unowned self] (username, userId) in
-//            print(username, userId)
+        AuthService.instance.registerUser(username: user.profile.givenName, email: user.profile.email, password: "gmailpassword ", completion: { (username, userId) in
+            //            print(username, userId)
             let storyBoard = UIStoryboard.init(name: "CreateRoom", bundle: nil)
             let createRoomVC = storyBoard.instantiateViewController(withIdentifier: "createRoomVC") as! CreateRoomViewController
-//            let storyBoard = UIStoryboard.init(name: "LiveRoom", bundle: nil)
-//            let createRoomVC = storyBoard.instantiateViewController(withIdentifier: "liveRoomVC") as! LiveRoomViewController
+            //            let storyBoard = UIStoryboard.init(name: "LiveRoom", bundle: nil)
+            //            let createRoomVC = storyBoard.instantiateViewController(withIdentifier: "liveRoomVC") as! LiveRoomViewController
             UserdataService.instance.setUserdata1(username: username, avatar: "avatar")
             self.navigationController?.pushViewController(createRoomVC, animated: true)
         })
@@ -78,7 +79,7 @@ class RegisterViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDe
             return
         }
         //  call Gmail uiDelegate and signindelegate
-        DispatchQueue.main.async { [weak self] in
+        DispatchQueue.main.async {
             GIDSignIn.sharedInstance().uiDelegate = self
             GIDSignIn.sharedInstance().delegate = self
         }
@@ -113,11 +114,11 @@ class RegisterViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDe
     
     func fbGraphRequest(completion: @escaping (Bool) -> ()) {
         let request = FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, gender, first_name, email"])
-        request?.start(completionHandler: { [unowned self] (connection, result, error) in
+        request?.start(completionHandler: { (connection, result, error) in
             guard let userInfo = result as? [String: Any] else { return }
             self.loginName = userInfo["first_name"]! as? String
             self.loginEmail = userInfo["email"]! as? String
-           completion(true)
+            completion(true)
         })
     }
     
@@ -130,17 +131,17 @@ class RegisterViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDe
     //    MARK: IBAction: RegularSignupButton
     
     @IBAction func signupClicked(_ sender: UIButton) {
-//        guard let usernametxt = username.text, let emailtxt = email.text, let passwordtxt = password.text,
-//        !usernametxt.isEmpty, !emailtxt.isEmpty, !passwordtxt.isEmpty else { return }
+        //        guard let usernametxt = usernameTextField.text, let emailtxt = emailTextField.text, let passwordtxt = passwordTextField.text,
+        //        !usernametxt.isEmpty, !emailtxt.isEmpty, !passwordtxt.isEmpty else { return }
         
-//        // Encryption
-//        let data = Data()
-//        let ciphertext = RNCryptor.encrypt(data: data, withPassword: passwordtxt)
-//        print(ciphertext)
+        //        // Encryption
+        //        let data = Data()
+        //        let ciphertext = RNCryptor.encrypt(data: data, withPassword: passwordtxt)
+        //        print(ciphertext)
         
-        AuthService.instance.registerUser(username: "james", email: "emailtxt", password: "passwordtxt") { (username, userId) in
+        AuthService.instance.registerUser(username: "bob", email: "bob@mail.com", password: "passwordtxt") { (username, userId) in
             if username != nil, userId != nil {
-//                UserdataService.instance.setUserdata1(username: username, avatar: "avatar")
+                //                UserdataService.instance.setUserdata1(username: username, avatar: "avatar")
                 
                 let storyBoard = UIStoryboard.init(name: "CreateRoom", bundle: nil)
                 let createRoomVC = storyBoard.instantiateViewController(withIdentifier: "createRoomVC") as! CreateRoomViewController
@@ -153,10 +154,10 @@ class RegisterViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDe
     
     //    MARK: IBAction: fbLoginButton
     @IBAction func fbLoginTapped(_ sender: UIButton) {
-        fbManagerSuccess{ [unowned self] (success) in
+        fbManagerSuccess{ (success) in
             if success {
                 AuthService.instance.registerUser(username: self.loginName!, email: self.loginEmail!, password: "facebookpassword ", completion: { (username, userId) in
-//                    print(username, userId)
+                    //                    print(username, userId)
                     let storyBoard = UIStoryboard.init(name: "CreateRoom", bundle: nil)
                     let createRoomVC = storyBoard.instantiateViewController(withIdentifier: "createRoomVC") as! CreateRoomViewController
                     self.navigationController?.pushViewController(createRoomVC, animated: true)
@@ -166,5 +167,4 @@ class RegisterViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDe
     }
     
 }
-
 
